a4e30d03a0f2dfa782f2604bca51fb10ffed2c9f
Claire <lee.claire156@gmail.com>
2023-02-14 15:19:55 -0500

    Edited product delete route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -95,23 +95,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
-    const productToDestroy = Product.destroy({
+    const productData = await Product.destroy({
       where: { id: req.params.id }
-    })
+    });
 
-    if (!productToDestroy) {
-      res.status(404).json({ message: 'No location found with this id!' });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id!' });
       return;
-    } else {
-      res.status(200).json(productToDestroy);
     }
+    res.status(200).json({message: 'Successfully deleted product.'});
+
 
   } catch (err) {
     res.status(500).json(err);
   }
+
 });
 
 module.exports = router;
